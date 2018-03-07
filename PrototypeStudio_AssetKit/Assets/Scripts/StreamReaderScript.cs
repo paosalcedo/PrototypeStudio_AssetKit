cbf17f697ddd69cacebb977f4e70bd75382eb20c
pao_s <paosalcedo@gmail.com>
2018-03-07 17:03:44 -0500

    Added new gitignore, probuilder

---
+++ PrototypeStudio_AssetKit/Assets/Scripts/StreamReaderScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class StreamReaderScript : MonoBehaviour
+{
+
+	public string[] fileNames;
+
+	public int levelNum = 0;
+	// Use this for initialization
+	void Start ()
+	{
+		string fileName = fileNames[levelNum];
+		string filePath = Application.dataPath + "/" + fileName;
+
+		StreamReader sr = new StreamReader(new MemoryStream((Resources.Load(fileName) as TextAsset).bytes));
+
+		while (!sr.EndOfStream)
+		{
+			string line = sr.ReadLine();
+
+			for (int i = 0; i < line.Length; i++)
+			{
+					
+			}
+		}
+		
+		sr.Close();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
