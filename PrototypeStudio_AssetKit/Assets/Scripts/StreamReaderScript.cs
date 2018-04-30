4cc5b8443aea15c1e5e57cb47159dc0b8ef974e9
pao_s <paosalcedo@gmail.com>
2018-04-30 02:32:53 -0400

    Added FSM to the Word class

--- PrototypeStudio_AssetKit/Assets/Scripts/StreamReaderScript.cs
+++ PrototypeStudio_AssetKit/Assets/Scripts/StreamReaderScript.cs
@@ -14,8 +14,7 @@ public class StreamReaderScript : MonoBehaviour
   	protected List<string> words = new List<string>();
 	protected List<GameObject> textmeshGOs = new List<GameObject>();
  	
-	// Use this for initialization
-	public void ReadPoem ()
+ 	public void ReadPoem ()
 	{
 		string fileName = fileNames[fileNum];
 		string path = Application.dataPath + "/" + fileName;
@@ -29,14 +28,19 @@ public class StreamReaderScript : MonoBehaviour
 
 			for (int i = 0; i < line.Length; i++)
 			{
+				//if it's not a blank space,
 				if (line[i] != ' ')
 				{
+					//or it's a letter or a character, append it to the string someWord
 					someWord = someWord + line[i].ToString();
 				} else if (line[i] == ' ')
 				{
- 					words.Add(someWord);					
-					someWord = "";	
-					
+					//if the StreamReader hits a blank space, it's probably the end of a word
+					//so instead of appending the blank space to someWord, 
+					//add it to the list of words to be projected into world space
+ 					words.Add(someWord);		
+					//clear the string someWord for the next characters			
+					someWord = "";		
 				}
 			}
 		}
--- PrototypeStudio_AssetKit/Assets/Scripts/Word.cs
+++ PrototypeStudio_AssetKit/Assets/Scripts/Word.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 
 public class Word : MonoBehaviour
 {
+	private FSM<Word> _fsm;
 	private bool hasCollider;
 
 	private GameObject wordsHolder;
@@ -13,19 +15,33 @@ public class Word : MonoBehaviour
 	{
 		wordsHolder = GameObject.Find("Words");
 		transform.SetParent(wordsHolder.transform);
+		_fsm = new FSM<Word>(this);
+		_fsm.TransitionTo<LookingAtPlayer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
- 
+		_fsm.Update();
 		//Look at player
-		if (GetPlayerDistance() > 100f)
-		{
-			LookAtPlayer();
-		}
+//		if (GetPlayerDistance() > 100f)
+//		{
+//			LookAtPlayer();
+//		}
+//
+//
+//		TurnOnColliderWhenPlayerIsNear();
+	}
 
+	private bool isTweenActive;
+	
+	private void SlowLookAtTween()
+	{
+ //		Sequence sequence = DOTween.Sequence();
+//		sequence.Append(transform.DORotate(2 * transform.position - Player.instance.gameObject.transform.position, 5f));
+//		sequence.OnComplete(() => isTweenActive = false);
 
-		TurnOnColliderWhenPlayerIsNear();
+		transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles,
+			2 * transform.position - Player.instance.gameObject.transform.position, Time.time);
 	}
 
 	private void TurnOnColliderWhenPlayerIsNear()
@@ -34,13 +50,7 @@ public class Word : MonoBehaviour
 		{
 			if (!hasCollider)
 			{
-				gameObject.AddComponent<BoxCollider>();
-				BoxCollider boxCollider = GetComponent<BoxCollider>();
-				boxCollider.size *= 2f;
-				boxCollider.size = new Vector3(boxCollider.size.x, boxCollider.size.y, 20f);
-//				gameObject.GetComponent<MeshCollider>().isTrigger = true;
-//				gameObject.GetComponent<MeshCollider>().convex = true;
-				hasCollider = true;
+				GenerateCollider();
 			}
 		}
 		else if (GetPlayerDistance() > 30f)
@@ -53,16 +63,77 @@ public class Word : MonoBehaviour
 		}
 	}
 
+	private void GenerateCollider()
+	{
+		gameObject.AddComponent<BoxCollider>();
+		BoxCollider boxCollider = GetComponent<BoxCollider>();
+		boxCollider.size *= 2f;
+		boxCollider.size = new Vector3(boxCollider.size.x, boxCollider.size.y, 20f);
+		hasCollider = true;
+	}
+
 	private void LookAtPlayer()
 	{
 		transform.LookAt(2 * transform.position - Player.instance.gameObject.transform.position);
 	}
 
-	protected float GetPlayerDistance()
+	private float GetPlayerDistance()
 	{
 		float playerDist;
 		playerDist = Vector3.Distance(Player.instance.transform.position, transform.position);
 		return playerDist;
+	}
+
+	private class WordState : FSM<Word>.State
+	{
+		
+	}
+
+	private class LookingAtPlayer : WordState
+	{
+		public override void OnEnter()
+		{
+			base.OnEnter();	
+//			Context.LookAtPlayer();
+		}
+
+		public override void Update()
+		{
+			base.Update();
+			if (Context.GetPlayerDistance() > 100)
+			{
+ 				Context.LookAtPlayer();
+			}
+			else
+			{
+				TransitionTo<ReadyForCollision>();	
+			}
+		}
+	}
+
+	private class ReadyForCollision : WordState
+	{
+		public override void OnEnter()
+		{
+			base.OnEnter();
+		}
+
+		public override void Update()
+		{
+			base.Update();
+			Context.TurnOnColliderWhenPlayerIsNear();
+
+			if (Context.GetPlayerDistance() >= 100)
+			{
+				TransitionTo<LookingAtPlayer>();
+			}
+		}
+
+		public override void OnExit()
+		{
+			base.OnExit();
+ 			Context.transform.DORotateQuaternion(Quaternion.LookRotation(Context.transform.position - Player.instance.gameObject.transform.position), 1f);
+ 		}
 	}
 
 }
